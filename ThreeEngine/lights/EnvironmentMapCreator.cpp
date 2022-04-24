0287072290d15163956c0191964eed3645cf1da3
feiy <feiy@nvidia.com>
2022-04-24 10:18:54 +0800

    adding back sse

--- ThreeEngine/lights/EnvironmentMapCreator.cpp
+++ ThreeEngine/lights/EnvironmentMapCreator.cpp
@@ -7,6 +7,43 @@
 
 const double PI = 3.14159265359;
 
+#if 1
+
+#include <xmmintrin.h>
+#include <mmintrin.h>
+
+void SHEval3(const float* pX, const float* pY, const float* pZ, float* pSH)
+{
+	__m128 fX, fY, fZ;
+	__m128 fC0, fC1, fS0, fS1, fTmpA, fTmpB, fTmpC;
+	fX = _mm_load_ps(pX);
+	fY = _mm_load_ps(pY);
+	fZ = _mm_load_ps(pZ);
+
+	__m128 fZ2 = _mm_mul_ps(fZ, fZ);
+
+	_mm_store_ps(pSH + 0 * 4, _mm_set_ps1(0.2820947917738781f));
+	_mm_store_ps(pSH + 2 * 4, _mm_mul_ps(_mm_set_ps1(0.4886025119029199f), fZ));
+	_mm_store_ps(pSH + 6 * 4, _mm_add_ps(_mm_mul_ps(_mm_set_ps1(0.9461746957575601f), fZ2), _mm_set_ps1(-0.3153915652525201f)));
+	fC0 = fX;
+	fS0 = fY;
+
+	fTmpA = _mm_set_ps1(-0.48860251190292f);
+	_mm_store_ps(pSH + 3 * 4, _mm_mul_ps(fTmpA, fC0));
+	_mm_store_ps(pSH + 1 * 4, _mm_mul_ps(fTmpA, fS0));
+	fTmpB = _mm_mul_ps(_mm_set_ps1(-1.092548430592079f), fZ);
+	_mm_store_ps(pSH + 7 * 4, _mm_mul_ps(fTmpB, fC0));
+	_mm_store_ps(pSH + 5 * 4, _mm_mul_ps(fTmpB, fS0));
+	fC1 = _mm_sub_ps(_mm_mul_ps(fX, fC0), _mm_mul_ps(fY, fS0));
+	fS1 = _mm_add_ps(_mm_mul_ps(fX, fS0), _mm_mul_ps(fY, fC0));
+
+	fTmpC = _mm_set_ps1(0.5462742152960395f);
+	_mm_store_ps(pSH + 8 * 4, _mm_mul_ps(fTmpC, fC1));
+	_mm_store_ps(pSH + 4 * 4, _mm_mul_ps(fTmpC, fS1));
+}
+
+#else
+
 inline void SHEval3(const float fX, const float fY, const float fZ, float* pSH)
 {
 	float fC0, fC1, fS0, fS1, fTmpA, fTmpB, fTmpC;
@@ -31,6 +68,8 @@ inline void SHEval3(const float fX, const float fY, const float fZ, float* pSH)
 	pSH[8] = fTmpC * fC1;
 	pSH[4] = fTmpC * fS1;
 }
+
+#endif
 
 EnvironmentMapCreator::EnvironmentMapCreator()
 {
@@ -79,45 +118,76 @@ void EnvironmentMapCreator::Create(int width, int height, const GLCubemap * cube
 	{
 		std::vector<uint8_t>& face = faces[i];
 
-		for (int j = 0; j < 128 * 128; j++)
+		for (int j = 0; j < 128 * 128; j+=4)
 		{
-			const uint8_t* pixel = &face[(size_t)j * 4];
-			glm::vec3 color = { (float)pixel[0] / 255.0f, (float)pixel[1] / 255.0f, (float)pixel[2] / 255.0f };
-			float col = -1.0f + ((float)(j % 128) + 0.5f) * pixelSize;
-			float row = -1.0f + ((float)(j / 128) + 0.5f) * pixelSize;
-			glm::vec3 coord;
-			switch (i)
-			{
-			case 0:
-				coord = { 1.0f, -row, -col };
-				break;
-			case 1:
-				coord = { -1.0f, -row, col };
-				break;
-			case 2:
-				coord = { col, 1, row };
-				break;
-			case 3:
-				coord = { col, -1, -row };
-				break;
-			case 4:
-				coord = { col, -row, 1 };
-				break;
-			case 5:
-				coord = { -col, -row, -1 };
-				break;
-			}
-			float lengthSq = glm::dot(coord, coord);
-			float weight = 4.0f / (sqrtf(lengthSq) * lengthSq);
-			totalWeight += weight;
-			glm::vec3 dir = glm::normalize(coord);
-			float shBasis[9];
-			SHEval3(dir.x, dir.y, dir.z, shBasis);
+			float dir_x[4];
+			float dir_y[4];
+			float dir_z[4];
+			float weight[4];
+			glm::vec3 color[4];
 
-			for (int k = 0; k < 9; k++)
+			for (int k = 0; k < 4; k++)
 			{
-				envMap->shCoefficients[k] += glm::vec4(shBasis[k] * weight * color, 0.0f);
+				int pixelIndex = j + k;
+
+				float col = -1.0f + ((float)(pixelIndex % 128) + 0.5f) * pixelSize;
+				float row = -1.0f + ((float)(pixelIndex / 128) + 0.5f) * pixelSize;
+				glm::vec3 coord;
+				switch (i)
+				{
+				case 0:
+					coord = { 1.0f, -row, -col };
+					break;
+				case 1:
+					coord = { -1.0f, -row, col };
+					break;
+				case 2:
+					coord = { col, 1, row };
+					break;
+				case 3:
+					coord = { col, -1, -row };
+					break;
+				case 4:
+					coord = { col, -row, 1 };
+					break;
+				case 5:
+					coord = { -col, -row, -1 };
+					break;
+				}
+				float lengthSq = glm::dot(coord, coord);
+				weight[k] = 4.0f / (sqrtf(lengthSq) * lengthSq);
+				totalWeight += weight[k];
+				glm::vec3 dir = glm::normalize(coord);
+				dir_x[k] = dir.x;
+				dir_y[k] = dir.y;
+				dir_z[k] = dir.z;
+
+				const uint8_t* pixel = &face[(size_t)pixelIndex * 4];
+				color[k] = { (float)pixel[0] / 255.0f, (float)pixel[1] / 255.0f, (float)pixel[2] / 255.0f };
 			}
+			
+#if 1
+			float shBasis[36];
+			SHEval3(dir_x, dir_y, dir_z, shBasis);
+
+			for (int k = 0; k < 36; k++)
+			{
+				int idx_col = k % 4;
+				int idx_coeff = k / 4;
+				envMap->shCoefficients[idx_coeff] += glm::vec4(shBasis[k] * weight[idx_col] * color[idx_col], 0.0f);
+			}
+#else
+			for (int k = 0; k < 4; k++)
+			{
+				float shBasis[9];
+				SHEval3(dir_x[k], dir_y[k], dir_z[k], shBasis);
+
+				for (int l = 0; l < 9; l++)
+				{
+					envMap->shCoefficients[l] += glm::vec4(shBasis[l] * weight[k] * color[k], 0.0f);
+				}
+			}
+#endif
 		}
 	}
 
