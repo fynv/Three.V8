57e71f929c751ec06730076585af77698b6d6756
feiy <feiy@nvidia.com>
2023-04-26 09:48:01 +0800

    Auto deploy webview2 runtime

--- QGameDev/main.cpp
+++ QGameDev/main.cpp
@@ -1,10 +1,28 @@
 #include <QStyleFactory>
+#include <QProcess>
+#include "WebView2Widget.h"
 #include "MainWindow.h"
 
 int main(int argc, char* argv[])
-{  
+{
     QApplication::setStyle("fusion");
     QApplication app(argc, argv);
+
+    LPWSTR version;
+    GetAvailableCoreWebView2BrowserVersionString(nullptr, &version);
+    if (version != nullptr)
+    {
+        CoTaskMemFree(version);
+    }
+    else
+    {
+        QString local_path = QCoreApplication::applicationDirPath();
+
+        QProcess proc;
+        proc.setProgram(local_path + "\\MicrosoftEdgeWebview2Setup.exe");
+        proc.start();
+        proc.waitForFinished();
+    } 
 
     MainWindow mainWnd(&app);
     mainWnd.showMaximized();
