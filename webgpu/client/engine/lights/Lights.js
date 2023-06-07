69a8bdc6b06eba8c0d1559049d0c5940f403204d
feiy <feiy@nvidia.com>
2023-06-07 17:37:43 +0800

    WebGPU: fix PCSS performance

--- webgpu/client/engine/lights/Lights.js
+++ webgpu/client/engine/lights/Lights.js
@@ -3,6 +3,106 @@ export function LightsIsEmpty(options)
     return options.directional_lights.length == 0;
 }
 
+let Poisson32_64 = new Float32Array([
+    -0.975402, -0.0711386,
+    -0.920347, -0.41142,
+    -0.883908, 0.217872,
+    -0.884518, 0.568041,
+    -0.811945, 0.90521,
+    -0.792474, -0.779962,
+    -0.614856, 0.386578,
+    -0.580859, -0.208777,
+    -0.53795, 0.716666,
+    -0.515427, 0.0899991,
+    -0.454634, -0.707938,
+    -0.420942, 0.991272,
+    -0.261147, 0.588488,
+    -0.211219, 0.114841,
+    -0.146336, -0.259194,
+    -0.139439, -0.888668,
+    0.0116886, 0.326395,
+    0.0380566, 0.625477,
+    0.0625935, -0.50853,
+    0.125584, 0.0469069,
+    0.169469, -0.997253,
+    0.320597, 0.291055,
+    0.359172, -0.633717,
+    0.435713, -0.250832,
+    0.507797, -0.916562,
+    0.545763, 0.730216,
+    0.56859, 0.11655,
+    0.743156, -0.505173,
+    0.736442, -0.189734,
+    0.843562, 0.357036,
+    0.865413, 0.763726,
+    0.872005, -0.927,
+
+    -0.934812, 0.366741,
+    -0.918943, -0.0941496,
+    -0.873226, 0.62389,
+    -0.8352, 0.937803,
+    -0.822138, -0.281655,
+    -0.812983, 0.10416,
+    -0.786126, -0.767632,
+    -0.739494, -0.535813,
+    -0.681692, 0.284707,
+    -0.61742, -0.234535,
+    -0.601184, 0.562426,
+    -0.607105, 0.847591,
+    -0.581835, -0.00485244,
+    -0.554247, -0.771111,
+    -0.483383, -0.976928,
+    -0.476669, -0.395672,
+    -0.439802, 0.362407,
+    -0.409772, -0.175695,
+    -0.367534, 0.102451,
+    -0.35313, 0.58153,
+    -0.341594, -0.737541,
+    -0.275979, 0.981567,
+    -0.230811, 0.305094,
+    -0.221656, 0.751152,
+    -0.214393, -0.0592364,
+    -0.204932, -0.483566,
+    -0.183569, -0.266274,
+    -0.123936, -0.754448,
+    -0.0859096, 0.118625,
+    -0.0610675, 0.460555,
+    -0.0234687, -0.962523,
+    -0.00485244, -0.373394,
+    0.0213324, 0.760247,
+    0.0359813, -0.0834071,
+    0.0877407, -0.730766,
+    0.14597, 0.281045,
+    0.18186, -0.529649,
+    0.188208, -0.289529,
+    0.212928, 0.063509,
+    0.23661, 0.566027,
+    0.266579, 0.867061,
+    0.320597, -0.883358,
+    0.353557, 0.322733,
+    0.404157, -0.651479,
+    0.410443, -0.413068,
+    0.413556, 0.123325,
+    0.46556, -0.176183,
+    0.49266, 0.55388,
+    0.506333, 0.876888,
+    0.535875, -0.885556,
+    0.615894, 0.0703452,
+    0.637135, -0.637623,
+    0.677236, -0.174291,
+    0.67626, 0.7116,
+    0.686331, -0.389935,
+    0.691031, 0.330729,
+    0.715629, 0.999939,
+    0.8493, -0.0485549,
+    0.863582, -0.85229,
+    0.890622, 0.850581,
+    0.898068, 0.633778,
+    0.92053, -0.355693,
+    0.933348, -0.62981,
+    0.95294, 0.156896
+]);
+
 export class Lights
 {
     constructor()
@@ -15,6 +115,8 @@ export class Lights
             magFilter: 'linear',
             minFilter: 'linear',            
         });   
+
+        this.constant_poisson = engine_ctx.createBuffer(Poisson32_64.buffer, GPUBufferUsage.UNIFORM, 0, (32+64)*2*4);
     }
 
     get_signature()
@@ -30,12 +132,17 @@ export class Lights
 
     get_options()
     {
+        let has_shadow = false;
         let directional_lights = [];
         for (let light of this.directional_lights)
         {
-            directional_lights.push(light.shadow!=null);
+            if (light.shadow!=null)
+            {
+                has_shadow = true;
+            }
+            directional_lights.push(light.shadow!=null);            
         }
-        return { directional_lights };
+        return { has_shadow, directional_lights };
     }
 
     clear_lists()
@@ -64,18 +171,31 @@ export class Lights
        
         let signature = JSON.stringify(options);
         if (!(signature in engine_ctx.cache.bindGroupLayouts.lights))
-        {
-            let entries = [
-                {
-                    binding: 0,
+        {            
+            let entries = [];
+            let binding = 0;
+
+            if (options.has_shadow)
+            {
+                entries.push({
+                    binding,
                     visibility: GPUShaderStage.FRAGMENT,
                     sampler:{
                         type: 'comparison',
                     }
-                },
-            ];
+                });
+                binding++;
 
-            let binding = 1;
+                entries.push({
+                    binding,
+                    visibility: GPUShaderStage.FRAGMENT,
+                    buffer:{
+                        type: "uniform"
+                    }
+                });
+                binding++;
+            }
+            
             for (let light of this.directional_lights)
             {
                 entries.push({
@@ -112,16 +232,28 @@ export class Lights
             }
 
             engine_ctx.cache.bindGroupLayouts.lights[signature] = engine_ctx.device.createBindGroupLayout({ entries });
-        }               
+        }   
+        
+        let entries = [];
+        let binding = 0;
 
-        let entries = [
-            {
+        if (options.has_shadow)
+        {
+            entries.push({
                 binding: 0,
                 resource: this.sampler
-            }
-        ];
+            });
+            binding++;
 
-        let binding = 1;
+            entries.push({
+                binding,
+                resource:{
+                    buffer: this.constant_poisson
+                }
+            });
+            binding++;
+        }
+       
         for (let light of this.directional_lights)
         {
             entries.push({
--- webgpu/client/engine/renderers/routines/StandardRoutine.js
+++ webgpu/client/engine/renderers/routines/StandardRoutine.js
@@ -322,8 +322,36 @@ fn BRDF_GGX(lightDir: vec3f,  viewDir: vec3f, normal: vec3f, f0: vec3f, f90: f32
 	return F*(V*D);
 }
 
+#if ${lights_options.has_shadow}
 @group(2) @binding(0)
 var uShadowSampler: sampler_comparison;
+
+@group(2) @binding(1)
+var<uniform> uPoisson32_64: array<vec4f, 48>;
+
+fn get_poisson_32(i: i32) -> vec2f
+{
+    if (i%2==0)
+    {
+        return uPoisson32_64[i/2].xy;
+    }
+    else
+    {
+        return uPoisson32_64[i/2].zw;
+    }
+}
+
+fn get_poisson_64(i: i32) -> vec2f
+{
+    if (i%2==0)
+    {
+        return uPoisson32_64[16 + i/2].xy;
+    }
+    else
+    {
+        return uPoisson32_64[16 + i/2].zw;
+    }
+}
 
 fn borderDepthTexture(shadowTex: texture_depth_2d, uv: vec2f) -> f32
 {
@@ -337,115 +365,13 @@ fn borderPCFTexture(shadowTex: texture_depth_2d, uvz : vec3f) -> f32
         ((uvz.x <= 1.0) && (uvz.y <= 1.0) &&  (uvz.x >= 0.0) && (uvz.y >= 0.0)));
 }
 
-const Poisson32 = array(
-    vec2(-0.975402, -0.0711386),
-    vec2(-0.920347, -0.41142),
-    vec2(-0.883908, 0.217872),
-    vec2(-0.884518, 0.568041),
-    vec2(-0.811945, 0.90521),
-    vec2(-0.792474, -0.779962),
-    vec2(-0.614856, 0.386578),
-    vec2(-0.580859, -0.208777),
-    vec2(-0.53795, 0.716666),
-    vec2(-0.515427, 0.0899991),
-    vec2(-0.454634, -0.707938),
-    vec2(-0.420942, 0.991272),
-    vec2(-0.261147, 0.588488),
-    vec2(-0.211219, 0.114841),
-    vec2(-0.146336, -0.259194),
-    vec2(-0.139439, -0.888668),
-    vec2(0.0116886, 0.326395),
-    vec2(0.0380566, 0.625477),
-    vec2(0.0625935, -0.50853),
-    vec2(0.125584, 0.0469069),
-    vec2(0.169469, -0.997253),
-    vec2(0.320597, 0.291055),
-    vec2(0.359172, -0.633717),
-    vec2(0.435713, -0.250832),
-    vec2(0.507797, -0.916562),
-    vec2(0.545763, 0.730216),
-    vec2(0.56859, 0.11655),
-    vec2(0.743156, -0.505173),
-    vec2(0.736442, -0.189734),
-    vec2(0.843562, 0.357036),
-    vec2(0.865413, 0.763726),
-    vec2(0.872005, -0.927)
-);
-
-const Poisson64 = array(
-    vec2(-0.934812, 0.366741),
-    vec2(-0.918943, -0.0941496),
-    vec2(-0.873226, 0.62389),
-    vec2(-0.8352, 0.937803),
-    vec2(-0.822138, -0.281655),
-    vec2(-0.812983, 0.10416),
-    vec2(-0.786126, -0.767632),
-    vec2(-0.739494, -0.535813),
-    vec2(-0.681692, 0.284707),
-    vec2(-0.61742, -0.234535),
-    vec2(-0.601184, 0.562426),
-    vec2(-0.607105, 0.847591),
-    vec2(-0.581835, -0.00485244),
-    vec2(-0.554247, -0.771111),
-    vec2(-0.483383, -0.976928),
-    vec2(-0.476669, -0.395672),
-    vec2(-0.439802, 0.362407),
-    vec2(-0.409772, -0.175695),
-    vec2(-0.367534, 0.102451),
-    vec2(-0.35313, 0.58153),
-    vec2(-0.341594, -0.737541),
-    vec2(-0.275979, 0.981567),
-    vec2(-0.230811, 0.305094),
-    vec2(-0.221656, 0.751152),
-    vec2(-0.214393, -0.0592364),
-    vec2(-0.204932, -0.483566),
-    vec2(-0.183569, -0.266274),
-    vec2(-0.123936, -0.754448),
-    vec2(-0.0859096, 0.118625),
-    vec2(-0.0610675, 0.460555),
-    vec2(-0.0234687, -0.962523),
-    vec2(-0.00485244, -0.373394),
-    vec2(0.0213324, 0.760247),
-    vec2(0.0359813, -0.0834071),
-    vec2(0.0877407, -0.730766),
-    vec2(0.14597, 0.281045),
-    vec2(0.18186, -0.529649),
-    vec2(0.188208, -0.289529),
-    vec2(0.212928, 0.063509),
-    vec2(0.23661, 0.566027),
-    vec2(0.266579, 0.867061),
-    vec2(0.320597, -0.883358),
-    vec2(0.353557, 0.322733),
-    vec2(0.404157, -0.651479),
-    vec2(0.410443, -0.413068),
-    vec2(0.413556, 0.123325),
-    vec2(0.46556, -0.176183),
-    vec2(0.49266, 0.55388),
-    vec2(0.506333, 0.876888),
-    vec2(0.535875, -0.885556),
-    vec2(0.615894, 0.0703452),
-    vec2(0.637135, -0.637623),
-    vec2(0.677236, -0.174291),
-    vec2(0.67626, 0.7116),
-    vec2(0.686331, -0.389935),
-    vec2(0.691031, 0.330729),
-    vec2(0.715629, 0.999939),
-    vec2(0.8493, -0.0485549),
-    vec2(0.863582, -0.85229),
-    vec2(0.890622, 0.850581),
-    vec2(0.898068, 0.633778),
-    vec2(0.92053, -0.355693),
-    vec2(0.933348, -0.62981),
-    vec2(0.95294, 0.156896)
-);
-
 fn findBlocker(shadowTex: texture_depth_2d, uv : vec2f, z: f32, searchRegionRadiusUV: vec2f) -> vec3f
 {
     var ret = vec3(0.0, 0.0, 32.0);
 
     for (var i=0; i<32; i++)
     {
-        let offset = Poisson32[i] * searchRegionRadiusUV;
+        let offset = get_poisson_32(i) * searchRegionRadiusUV;
         let shadowMapDepth = borderDepthTexture(shadowTex, uv + offset);
         if (shadowMapDepth<z)
         {
@@ -463,7 +389,7 @@ fn pcfFilter(shadowTex: texture_depth_2d, uv : vec2f, z: f32,  filterRadiusUV: v
 
     for (var i=0; i<64; i++)
     {
-        let offset = Poisson64[i] * filterRadiusUV;
+        let offset = get_poisson_64(i) * filterRadiusUV;
         sum += borderPCFTexture(shadowTex, vec3(uv + offset, z));
     }    
 
@@ -493,10 +419,12 @@ fn computePCSSShadowCoef(shadow: DirectionalShadow, zEye: f32, uvz : vec3f, shad
     return pcfFilter(shadowTex, uv, z, penumbraRadius);
 }
 
+#endif
+
 
 ${(()=>{
     let code = "";
-    let binding = 1;
+    let binding = lights_options.has_shadow? 2: 0;
     for (let i=0; i<directional_lights.length; i++)
     {        
         let has_shadow = directional_lights[i];
